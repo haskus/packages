0f10fdea7d57edacb91b0e13de3eb3e326ab820b
Sylvain Henry <sylvain@haskus.fr>
2017-07-21 23:24:12 +0200

    List: export Data.List.Extra from extra

--- src/lib/Haskus/Utils/List.hs
+++ src/lib/Haskus/Utils/List.hs
@@ -1,10 +1,12 @@
 module Haskus.Utils.List
    ( checkLength
    , module Data.List
+   , module Data.List.Extra
    )
 where
 
 import Data.List
+import Data.List.Extra
 
 -- | Check that a list has the given length (support infinite lists)
 checkLength :: Word -> [a] -> Bool
