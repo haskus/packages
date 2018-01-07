a0287fe652a442feaa2dd2f5f81df5aec9bed498
Sylvain HENRY <hsyl20@gmail.com>
2018-01-07 23:20:19 +0100

    Char8: add some instances

--- src/lib/Haskus/Format/Binary/Char.hs
+++ src/lib/Haskus/Format/Binary/Char.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+-- | Character
 module Haskus.Format.Binary.Char
    ( Char8 (..)
    )
 where
 
 import Haskus.Format.Binary.Word
+import Haskus.Format.Binary.Storable
 
 -- | 8-bit character (ASCII, etc.)
-newtype Char8 = Char8 Word8
+newtype Char8
+   = Char8 Word8
+   deriving (Show,Eq,Ord,Storable)
