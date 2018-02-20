afdee7fe269cc44ff1427bd20ccbe1fdf030612e
Sylvain Henry <sylvain@haskus.fr>
2018-02-20 13:50:25 +0100

    Fix tests (new Variant API)

--- src/tests/Haskus/Tests/Utils/Variant.hs
+++ src/tests/Haskus/Tests/Utils/Variant.hs
@@ -28,7 +28,7 @@ type ABC = Variant '[A,B,C]
 type DEF = Variant '[D,E,F]
 
 b :: ABC
-b = setVariantN @1 B
+b = toVariantAt @1 B
 
 b2d :: B -> D
 b2d = const D
@@ -37,92 +37,92 @@ c2d :: C -> D
 c2d = const D
 
 b2def :: B -> DEF
-b2def = const (setVariant E)
+b2def = const (toVariant E)
 
 c2def :: C -> DEF
-c2def = const (setVariant E)
+c2def = const (toVariant E)
 
 
 testsVariant :: TestTree
 testsVariant = testGroup "Variant" $
    [ testProperty "set/get by index (match)"
-         (getVariantN @1 b == Just B)
+         (fromVariantAt @1 b == Just B)
    , testProperty "set/get by index (dont' match)"
-         (getVariantN @0 b == Nothing)
+         (fromVariantAt @0 b == Nothing)
    , testProperty "set/get by type (match)"
-         (getVariant    (setVariant B :: ABC) == Just B)
+         (fromVariant    (toVariant B :: ABC) == Just B)
    , testProperty "set/get by type (don't match)"
-         (getVariant @C (setVariant B :: ABC) == Nothing)
+         (fromVariant @C (toVariant B :: ABC) == Nothing)
 
    , testProperty "variant equality (match)"
          (b == b)
    , testProperty "variant equality (don't match)"
-         (b /= setVariant C)
+         (b /= toVariant C)
 
    , testProperty "update by index (match)"
-         (updateVariantN @1 (const D) b == setVariantN @1 D)
+         (updateVariantAt @1 (const D) b == toVariantAt @1 D)
    , testProperty "update by index (don't match)"
-         (updateVariantN @0 (const F) b == setVariantN @1 B)
+         (updateVariantAt @0 (const F) b == toVariantAt @1 B)
    , testProperty "update by type (match)"
-         (updateVariant b2d b == setVariantN @1 D)
+         (updateVariantFirst b2d b == toVariantAt @1 D)
    , testProperty "update by type (don't match)"
-         (updateVariant c2d b == setVariant B)
+         (updateVariantFirst c2d b == toVariant B)
    , testProperty "update/fold by index (match)"
-         (updateVariantFoldN @1 b2def b == setVariant E)
+         (updateVariantFoldN @1 b2def b == toVariant E)
    , testProperty "update/fold by index (don't match)"
-         (updateVariantFoldN @2 c2def b == setVariant B)
+         (updateVariantFoldN @2 c2def b == toVariant B)
 
    , testProperty "Convert into tuple"
          (variantToTuple b == (Nothing, Just B, Nothing))
    , testProperty "Convert single variant"
-         (singleVariant (setVariant A :: Variant '[A]) == A)
+         (variantToValue (toVariant A :: Variant '[A]) == A)
 
    , testProperty "Lift Either: Left"
-         (liftEither (Left A :: Either A B) == setVariant A)
+         (variantFromEither (Left A :: Either A B) == toVariant A)
    , testProperty "Lift Either: Right"
-         (liftEither (Right B :: Either A B) == setVariant B)
+         (variantFromEither (Right B :: Either A B) == toVariant B)
 
    , testProperty "To Either: Left"
-         (toEither (setVariant B :: Variant '[A,B]) == Left B)
+         (variantToEither (toVariant B :: Variant '[A,B]) == Left B)
    , testProperty "To Either: Right"
-         (toEither (setVariant A :: Variant '[A,B]) == Right A)
+         (variantToEither (toVariant A :: Variant '[A,B]) == Right A)
 
-   , testProperty "headVariant (match)"
-         (headVariant (setVariant A :: ABC) == Right A)
-   , testProperty "headVariant (don't match)"
-         (isLeft (headVariant b))
+   , testProperty "popVariantHead (match)"
+         (popVariantHead (toVariant A :: ABC) == Right A)
+   , testProperty "popVariantHead (don't match)"
+         (isLeft (popVariantHead b))
 
-   , testProperty "pickVariant (match)"
-         (pickVariant @1 b == Right B)
-   , testProperty "pickVariant (don't match)"
-         (isLeft (pickVariant @2 b))
+   , testProperty "popVariantAt (match)"
+         (popVariantAt @1 b == Right B)
+   , testProperty "popVariantAt (don't match)"
+         (isLeft (popVariantAt @2 b))
 
-   , testProperty "catchVariant (match)"
-         (catchVariant @D (setVariantN @4 D :: Variant '[A,B,C,B,D,E,D]) == Right D)
-   , testProperty "catchVariant (match)"
-         (catchVariant @D (setVariantN @6 D :: Variant '[A,B,C,B,D,E,D]) == Right D)
-   , testProperty "catchVariant (don't match)"
-         (catchVariant @B (setVariantN @4 D :: Variant '[A,B,C,B,D,E,D]) == Left (setVariantN @2 D))
+   , testProperty "popVariant (match)"
+         (popVariant @D (toVariantAt @4 D :: Variant '[A,B,C,B,D,E,D]) == Right D)
+   , testProperty "popVariant (match)"
+         (popVariant @D (toVariantAt @6 D :: Variant '[A,B,C,B,D,E,D]) == Right D)
+   , testProperty "popVariant (don't match)"
+         (popVariant @B (toVariantAt @4 D :: Variant '[A,B,C,B,D,E,D]) == Left (toVariantAt @2 D))
 
    , testProperty "prependVariant"
-         (getVariantN @4 (prependVariant @'[D,E,F] b) == Just B)
+         (fromVariantAt @4 (prependVariant @'[D,E,F] b) == Just B)
    , testProperty "appendVariant"
-         (getVariantN @1 (appendVariant @'[D,E,F] b)  == Just B)
+         (fromVariantAt @1 (appendVariant @'[D,E,F] b)  == Just B)
 
    , testProperty "alterVariant"
-         (alterVariant @Num (+1) (setVariant (1.0 :: Float) :: Variant '[Int,Float]) == setVariant (2.0 :: Float))
+         (alterVariant @Num (+1) (toVariant (1.0 :: Float) :: Variant '[Int,Float]) == toVariant (2.0 :: Float))
    , testProperty "alterVariant"
-         (alterVariant @Num (+1) (setVariant (1.0 :: Float) :: Variant '[Float,Int]) == setVariant (2.0 :: Float))
+         (alterVariant @Num (+1) (toVariant (1.0 :: Float) :: Variant '[Float,Int]) == toVariant (2.0 :: Float))
 
    , testProperty "traverseVariant"
          (traverseVariant @OrdNum (\x -> if x > 1 then Just x else Nothing)
-            (setVariant (2.0 :: Float) :: Variant '[Float,Int]) == Just (setVariant (2.0 :: Float)))
+            (toVariant (2.0 :: Float) :: Variant '[Float,Int]) == Just (toVariant (2.0 :: Float)))
    , testProperty "traverseVariant"
          (traverseVariant @OrdNum (\x -> if x > 1 then Just x else Nothing)
-            (setVariant (0.5 :: Float) :: Variant '[Float,Int]) == Nothing)
+            (toVariant (0.5 :: Float) :: Variant '[Float,Int]) == Nothing)
 
    , testProperty "liftVariant"
-         (getVariant (liftVariant b :: Variant '[D,A,E,B,F,C])  == Just B)
+         (fromVariant (liftVariant b :: Variant '[D,A,E,B,F,C])  == Just B)
    ]
 
 class (Ord a, Num a) => OrdNum a
