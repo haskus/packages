80681e2c9793ab30baaf4e28669d9cddec6d19e1
Sylvain Henry <sylvain@haskus.fr>
2019-01-10 01:59:01 +0100

    Add bufferSize

--- haskus-binary/src/lib/Haskus/Memory/Buffer.hs
+++ haskus-binary/src/lib/Haskus/Memory/Buffer.hs
@@ -17,6 +17,7 @@ module Haskus.Memory.Buffer
    , Management (..)
    , Mutability (..)
    , withBufferAddr#
+   , bufferSize
    -- * Finalizers
    , addFinalizer
    , makeFinalizable
@@ -26,7 +27,6 @@ where
 
 import qualified Data.Primitive.ByteArray as BA
 import qualified Data.Primitive.Types     as BA
-import Data.Void
 import Control.Monad.Primitive
 import Control.Monad
 import Data.IORef
@@ -54,16 +54,16 @@ data Mutability
    deriving (Show,Eq)
 
 data Buffer s (mut :: Mutability) (pin :: Pinning) (gc :: Management) where
-   Buffer    :: BA.ByteArray                        -> Buffer Void 'Immutable 'NotPinned 'Managed
-   BufferP   :: BA.ByteArray                        -> Buffer Void 'Immutable 'Pinned    'Managed
-   BufferM   :: BA.MutableByteArray s               -> Buffer s    'Mutable   'NotPinned 'Managed
-   BufferMP  :: BA.MutableByteArray s               -> Buffer s    'Mutable   'Pinned    'Managed
-   BufferE   :: Addr#                               -> Buffer s    'Mutable   'Pinned    'NotManaged
-   BufferF   :: BA.ByteArray          -> Finalizers -> Buffer Void 'Immutable 'NotPinned 'Finalized
-   BufferPF  :: BA.ByteArray          -> Finalizers -> Buffer Void 'Immutable 'Pinned    'Finalized
-   BufferMF  :: BA.MutableByteArray s -> Finalizers -> Buffer s    'Mutable   'NotPinned 'Finalized
-   BufferMPF :: BA.MutableByteArray s -> Finalizers -> Buffer s    'Mutable   'Pinned    'Finalized
-   BufferEF  :: Addr#                 -> Finalizers -> Buffer s    'Mutable   'Pinned    'Finalized
+   Buffer    :: BA.ByteArray                        -> Buffer s 'Immutable 'NotPinned 'Managed
+   BufferP   :: BA.ByteArray                        -> Buffer s 'Immutable 'Pinned    'Managed
+   BufferM   :: BA.MutableByteArray s               -> Buffer s 'Mutable   'NotPinned 'Managed
+   BufferMP  :: BA.MutableByteArray s               -> Buffer s 'Mutable   'Pinned    'Managed
+   BufferE   :: Addr# -> Word                       -> Buffer s 'Mutable   'Pinned    'NotManaged
+   BufferF   :: BA.ByteArray          -> Finalizers -> Buffer s 'Immutable 'NotPinned 'Finalized
+   BufferPF  :: BA.ByteArray          -> Finalizers -> Buffer s 'Immutable 'Pinned    'Finalized
+   BufferMF  :: BA.MutableByteArray s -> Finalizers -> Buffer s 'Mutable   'NotPinned 'Finalized
+   BufferMPF :: BA.MutableByteArray s -> Finalizers -> Buffer s 'Mutable   'Pinned    'Finalized
+   BufferEF  :: Addr# -> Word         -> Finalizers -> Buffer s 'Mutable   'Pinned    'Finalized
 
 -- | A buffer with an additional phantom type indicating its binary format
 newtype TypedBuffer (t :: k) s mut pin gc = TypedBuffer (Buffer s mut pin gc)
@@ -100,7 +100,7 @@ insertFinalizer (Finalizers rfs) f = do
 -- | Add a finalizer.
 addFinalizer :: Buffer s mut pin 'Finalized -> IO () -> IO ()
 addFinalizer b f = case b of
-   BufferEF _addr fin@(Finalizers rfs) -> do
+   BufferEF _addr _sz fin@(Finalizers rfs) -> do
       wasEmpty <- insertFinalizer fin f
       -- add the weak reference to the finalizer IORef (not to Addr#)
       when wasEmpty $ void $ mkWeakIORef rfs (runFinalizers fin)
@@ -138,39 +138,39 @@ newFinalizers = Finalizers <$> newIORef []
 
 -- | Touch a buffer
 touchBuffer :: PrimMonad m => Buffer s mut pin gc -> m ()
-touchBuffer (Buffer    ba                    ) = touch ba
-touchBuffer (BufferP   ba                    ) = touch ba
-touchBuffer (BufferM   ba                    ) = touch ba
-touchBuffer (BufferMP  ba                    ) = touch ba
-touchBuffer (BufferF   ba     _fin           ) = touch ba
-touchBuffer (BufferPF  ba     _fin           ) = touch ba
-touchBuffer (BufferMF  ba     _fin           ) = touch ba
-touchBuffer (BufferMPF ba     _fin           ) = touch ba
-touchBuffer (BufferE   _addr                 ) = return ()
-touchBuffer (BufferEF  _addr (Finalizers fin)) = touch fin
+touchBuffer (Buffer    ba                        ) = touch ba
+touchBuffer (BufferP   ba                        ) = touch ba
+touchBuffer (BufferM   ba                        ) = touch ba
+touchBuffer (BufferMP  ba                        ) = touch ba
+touchBuffer (BufferF   ba         _fin           ) = touch ba
+touchBuffer (BufferPF  ba         _fin           ) = touch ba
+touchBuffer (BufferMF  ba         _fin           ) = touch ba
+touchBuffer (BufferMPF ba         _fin           ) = touch ba
+touchBuffer (BufferE   _addr _sz                 ) = return ()
+touchBuffer (BufferEF  _addr _sz (Finalizers fin)) = touch fin
 
 -- | Make a buffer finalizable
 --
 -- The new buffer liveness is used to trigger finalizers.
 --
 makeFinalizable :: Buffer s mut pin f -> IO (Buffer s mut pin 'Finalized)
-makeFinalizable (BufferE addr)   = BufferEF  addr <$> newFinalizers
-makeFinalizable (Buffer  ba  )   = BufferF   ba   <$> newFinalizers
-makeFinalizable (BufferP ba  )   = BufferPF  ba   <$> newFinalizers
-makeFinalizable (BufferM ba  )   = BufferMF  ba   <$> newFinalizers
-makeFinalizable (BufferMP ba )   = BufferMPF ba   <$> newFinalizers
-makeFinalizable x@(BufferF {})   = return x
-makeFinalizable x@(BufferEF {})  = return x
-makeFinalizable x@(BufferPF {})  = return x
-makeFinalizable x@(BufferMF {})  = return x
-makeFinalizable x@(BufferMPF {}) = return x
+makeFinalizable (BufferE addr sz) = BufferEF  addr sz <$> newFinalizers
+makeFinalizable (Buffer  ba  )    = BufferF   ba      <$> newFinalizers
+makeFinalizable (BufferP ba  )    = BufferPF  ba      <$> newFinalizers
+makeFinalizable (BufferM ba  )    = BufferMF  ba      <$> newFinalizers
+makeFinalizable (BufferMP ba )    = BufferMPF ba      <$> newFinalizers
+makeFinalizable x@(BufferF {})    = return x
+makeFinalizable x@(BufferEF {})   = return x
+makeFinalizable x@(BufferPF {})   = return x
+makeFinalizable x@(BufferMF {})   = return x
+makeFinalizable x@(BufferMPF {})  = return x
 
 
 -- | Do something with a buffer address
 --
 -- Note: don't write into immutable buffer as it would break referential
 -- consistency
-withBufferAddr# :: Buffer s mut 'Pinned f -> (Addr# -> IO a) -> IO a
+withBufferAddr# :: Buffer s mut 'Pinned gc -> (Addr# -> IO a) -> IO a
 withBufferAddr# b@(BufferP ba) f = do
    let !(BA.Addr addr) = BA.byteArrayContents ba
    r <- f addr
@@ -191,8 +191,22 @@ withBufferAddr# b@(BufferMPF ba _fin) f = do
    r <- f addr
    touch b
    return r
-withBufferAddr# (BufferE  addr)        f = f (addr)
-withBufferAddr# b@(BufferEF addr _fin) f = do
+withBufferAddr# (BufferE addr _sz)         f = f (addr)
+withBufferAddr# b@(BufferEF addr _sz _fin) f = do
    r <- f addr
    touch b
    return r
+
+
+-- | Get buffer size
+bufferSize :: (PrimState m ~ s, PrimMonad m) => Buffer s mut pin gc -> m Word
+bufferSize (BufferM ba  )           = fromIntegral <$> BA.getSizeofMutableByteArray ba
+bufferSize (BufferMP ba )           = fromIntegral <$> BA.getSizeofMutableByteArray ba
+bufferSize (BufferMF  ba _fin)      = fromIntegral <$> BA.getSizeofMutableByteArray ba
+bufferSize (BufferMPF ba _fin)      = fromIntegral <$> BA.getSizeofMutableByteArray ba
+bufferSize (BufferE  _addr sz     ) = return sz
+bufferSize (BufferEF _addr sz _fin) = return sz
+bufferSize (Buffer  ba  )           = return $ fromIntegral $ BA.sizeofByteArray ba
+bufferSize (BufferP ba  )           = return $ fromIntegral $ BA.sizeofByteArray ba
+bufferSize (BufferF   ba _fin)      = return $ fromIntegral $ BA.sizeofByteArray ba
+bufferSize (BufferPF  ba _fin)      = return $ fromIntegral $ BA.sizeofByteArray ba
