be3602766dc635e107bd91f8def51f0da93ba3d1
Sylvain Henry <sylvain@haskus.fr>
2019-01-10 15:02:58 +0100

    Add CodePoint

--- haskus-binary/src/lib/Haskus/Format/Text/Unicode.hs
+++ haskus-binary/src/lib/Haskus/Format/Text/Unicode.hs
@@ -1,5 +1,6 @@
 module Haskus.Format.Text.Unicode
-   ( UTF8 (..)
+   ( CodePoint (..)
+   , UTF8 (..)
    , UTF16_BOM (..)
    , UTF16_BE (..)
    , UTF16_LE (..)
@@ -9,6 +10,9 @@ module Haskus.Format.Text.Unicode
    )
 where
 
+import Haskus.Format.Binary.Word
+import Numeric
+
 data UTF8      = UTF8
 data UTF16_BOM = UTF16_BOM
 data UTF16_BE  = UTF16_BE
@@ -16,3 +20,28 @@ data UTF16_LE  = UTF16_LE
 data UTF32_BOM = UTF32_BOM
 data UTF32_BE  = UTF32_BE
 data UTF32_LE  = UTF32_LE
+
+-- | Code point
+--
+-- Number from 0 to 0x10FFFF
+newtype CodePoint = CodePoint Word32 deriving (Eq)
+
+-- | Show instance for CodePoint
+--
+-- >>> CodePoint 0x1234
+-- U+1234
+--
+-- >>> CodePoint 0x12
+-- U+0012
+--
+-- >>> CodePoint 0x1234A
+-- U+1234A
+--
+instance Show CodePoint where
+   show (CodePoint v) = "U+" ++ f (showHex v "")
+      where
+         f xs@[_,_,_] = '0':xs
+         f xs@[_,_]   = "00"   <> xs
+         f xs@[_]     = "000"  <> xs
+         f xs@[]      = "0000" <> xs
+         f xs         = xs
