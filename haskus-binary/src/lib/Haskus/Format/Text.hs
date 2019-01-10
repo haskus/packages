d1cfdb6fec0445d77a99455c37d33a5b5a8bef1c
Sylvain Henry <sylvain@haskus.fr>
2019-01-10 11:59:43 +0100

    Add support for OverloadedStrings

--- haskus-binary/src/lib/Haskus/Format/Text.hs
+++ haskus-binary/src/lib/Haskus/Format/Text.hs
@@ -14,12 +14,16 @@ module Haskus.Format.Text
    -- * Text
    , TextFormat (..)
    , TextBuffer (..)
+   , TextI
+   , TextB
    , ShowText (..)
    )
 where
 
 import qualified Data.Text as T
 import Data.Char
+import Data.String
+import GHC.Exts (IsList(..))
 
 import Haskus.Format.Binary.Word
 import Haskus.Memory.Buffer
@@ -114,8 +118,16 @@ data TextFormat
    | UTF32_LE
    deriving (Show,Eq)
 
+-- | Text buffer
+--
+-- The buffer contains a text encoded according to phantom type t
 newtype TextBuffer (t :: k) b
    = TextBuffer b
+
+-- type aliases
+type TextI t            = TextBuffer t BufferI
+type TextB t mut pin gc = TextBuffer t (Buffer mut pin gc)
+
 
 class ShowText t b where
    -- | Show text in IO
@@ -138,3 +150,13 @@ instance ShowText 'ASCII (Buffer mut pin gc) where
 
    showText (TextBuffer b) =
       bufferToList b ||> fromIntegral ||> chr
+
+-- | Support ASCII text with OverloadedStrings
+--
+-- >>> :set -XOverloadedStrings
+-- >>> let t = "HELLO" :: TextI 'ASCII
+-- >>> showText t
+-- "HELLO"
+--
+instance (IsList b, Item b ~ Word8) => IsString (TextBuffer 'ASCII b) where
+   fromString s = TextBuffer (fromList (fmap (fromIntegral . ord) s))
