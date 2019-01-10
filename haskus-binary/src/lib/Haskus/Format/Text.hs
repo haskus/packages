fe6e5e4ccddcb29f71665a2fb03f0ae827024364
Sylvain Henry <sylvain@haskus.fr>
2019-01-10 13:34:16 +0100

    Remove dependency on text

--- haskus-binary/src/lib/Haskus/Format/Text.hs
+++ haskus-binary/src/lib/Haskus/Format/Text.hs
@@ -8,11 +8,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module Haskus.Format.Text
-   ( Text
-   , ToText (..)
-   , toTextShow
-   -- * Text
-   , TextFormat (..)
+   ( TextFormat (..)
    , TextBuffer (..)
    , TextI
    , TextB
@@ -20,7 +16,6 @@ module Haskus.Format.Text
    )
 where
 
-import qualified Data.Text as T
 import Data.Char
 import Data.String
 import GHC.Exts (IsList(..))
@@ -34,77 +29,6 @@ import Haskus.Utils.Flow
 -- >>> :set -XTypeApplications
 -- >>> :set -XFlexibleContexts
 -- >>> :set -XTypeFamilies
-
--- | UTF-16 encoded strict text
-type Text = T.Text
-
--- | Value convertible into text
-class ToText a where
-   toText :: a -> Text
-
-
-instance ToText String where
-   toText x = T.pack x 
-
-instance ToText Text where
-   toText x = x
-
-instance ToText Float where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-instance ToText Double where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-
-instance ToText Int where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-instance ToText Int8 where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-instance ToText Int16 where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-instance ToText Int32 where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-instance ToText Int64 where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-
-instance ToText Word where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-instance ToText Word8 where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-instance ToText Word16 where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-instance ToText Word32 where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-instance ToText Word64 where
-   {-# INLINE toText #-}
-   toText = toTextShow
-
-
--- | Convert a Showable value into a Text
-toTextShow :: Show a => a -> Text
-{-# INLINE toTextShow #-}
-toTextShow x = T.pack (show x)
-
 
 -- | Supported text formats
 data TextFormat
