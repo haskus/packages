efe18b5956fdc779f65ffeac620a88891da70052
Sylvain Henry <sylvain@haskus.fr>
2019-01-11 19:11:30 +0100

    Add serializer into a mutable buffer

--- haskus-binary/src/lib/Haskus/Format/Binary/Serialize.hs
+++ haskus-binary/src/lib/Haskus/Format/Binary/Serialize.hs
@@ -19,29 +19,81 @@ import Haskus.Data.Buffer
 import Haskus.Format.Binary.Word
 import Haskus.Format.Binary.Endianness
 import Haskus.Utils.Types
+import Haskus.Utils.Monad
+
+import GHC.Exts (IsList(..))
 
 -- | Monad which can build a sequence of bytes
 class Monad m => PutMonad m where
+   -- | Write a Word8
+   putWord8 :: Word8 -> m ()
+   -- | Write a Word16
+   putWord16 :: Word16 -> m ()
+   -- | Write a Word32
+   putWord32 :: Word32 -> m ()
+   -- | Write a Word64
+   putWord64 :: Word64 -> m ()
+
+   -- | Write a Word16 with little-endian order
+   putWord16LE :: Word16 -> m ()
+   putWord16LE x = putWord16 (hostToLittleEndian x)
+   -- | Write a Word32 with little-endian order
+   putWord32LE :: Word32 -> m ()
+   putWord32LE x = putWord32 (hostToLittleEndian x)
+   -- | Write a Word64 with little-endian order
+   putWord64LE :: Word64 -> m ()
+   putWord64LE x = putWord64 (hostToLittleEndian x)
+
+   -- | Write a Word16 with big-endian order
+   putWord16BE :: Word16 -> m ()
+   putWord16BE x = putWord16 (hostToBigEndian x)
+   -- | Write a Word32 with big-endian order
+   putWord32BE :: Word32 -> m ()
+   putWord32BE x = putWord32 (hostToBigEndian x)
+   -- | Write a Word64 with big-endian order
+   putWord64BE :: Word64 -> m ()
+   putWord64BE x = putWord64 (hostToBigEndian x)
+
    -- | Write some Word8
    putWord8s   :: [Word8]  -> m ()
+   putWord8s xs = forM_ xs putWord8
+
    -- | Write some Word16
    putWord16s  :: [Word16] -> m ()
+   putWord16s xs = forM_ xs putWord16
+
    -- | Write some Word32
    putWord32s  :: [Word32] -> m ()
+   putWord32s xs = forM_ xs putWord32
+
    -- | Write some Word64
    putWord64s  :: [Word64] -> m ()
+   putWord64s xs = forM_ xs putWord64
+
    -- | Write some Word16 with little-endian order
    putWord16LEs  :: [Word16] -> m ()
+   putWord16LEs xs = putWord16s (fmap hostToLittleEndian xs)
+
    -- | Write some Word32 with little-endian order
    putWord32LEs  :: [Word32] -> m ()
+   putWord32LEs xs = putWord32s (fmap hostToLittleEndian xs)
+
    -- | Write some Word64 with little-endian order
    putWord64LEs :: [Word64] -> m ()
+   putWord64LEs xs = putWord64s (fmap hostToLittleEndian xs)
+
    -- | Write some Word16 with big-endian order
    putWord16BEs  :: [Word16] -> m ()
+   putWord16BEs xs = putWord16s (fmap hostToBigEndian xs)
+
    -- | Write some Word32 with big-endian order
    putWord32BEs  :: [Word32] -> m ()
+   putWord32BEs xs = putWord32s (fmap hostToBigEndian xs)
+
    -- | Write some Word64 with big-endian order
    putWord64BEs :: [Word64] -> m ()
+   putWord64BEs xs = putWord64s (fmap hostToBigEndian xs)
+
    -- | Write the contents of a buffer
    putBuffer   :: Buffer mut pin gc heap -> m ()
 
@@ -49,31 +101,76 @@ class Monad m => PutMonad m where
    --
    -- This is a hint for the putter to speed up the allocation of memory
    preAllocateAtLeast :: Word -> m ()
+   preAllocateAtLeast _ = return ()
 
 -- | Monad which can read a sequence of bytes
 class Monad m => GetMonad m where
+   -- | Read a Word8
+   getWord8    :: m Word8
+   -- | Read a Word16 with host endianness
+   getWord16   :: m Word16
+   -- | Read a Word32 with host endianness
+   getWord32   :: m Word32
+   -- | Read a Word64 with host endianness
+   getWord64   :: m Word64
+
+   -- | Read a Word16 with little-endian order
+   getWord16LE   :: m Word16
+   getWord16LE = littleEndianToHost <$> getWord16
+   -- | Read a Word32 with little-endian order
+   getWord32LE   :: m Word32
+   getWord32LE = littleEndianToHost <$> getWord32
+   -- | Read a Word64 with little-endian order
+   getWord64LE   :: m Word64
+   getWord64LE = littleEndianToHost <$> getWord64
+   -- | Read a Word16 with big-endian order
+   getWord16BE   :: m Word16
+   getWord16BE = bigEndianToHost <$> getWord16
+   -- | Read a Word32 with big-endian order
+   getWord32BE   :: m Word32
+   getWord32BE = bigEndianToHost <$> getWord32
+   -- | Read a Word64 with big-endian order
+   getWord64BE   :: m Word64
+   getWord64BE = bigEndianToHost <$> getWord64
+
    -- | Read some Word8
    getWord8s     :: Word -> m [Word8]
+   getWord8s n = replicateM (fromIntegral n) getWord8
    -- | Read some Word16 with host endianness
    getWord16s    :: Word -> m [Word16]
+   getWord16s n = replicateM (fromIntegral n) getWord16
    -- | Read some Word32 with host endianness
    getWord32s    :: Word -> m [Word32]
+   getWord32s n = replicateM (fromIntegral n) getWord32
    -- | Read some Word64 with host endianness
    getWord64s    :: Word -> m [Word64]
+   getWord64s n = replicateM (fromIntegral n) getWord64
+
    -- | Read some Word16 with little-endian order
    getWord16LEs    :: Word -> m [Word16]
+   getWord16LEs n = replicateM (fromIntegral n) getWord16LE
    -- | Read some Word32 with little-endian order
    getWord32LEs    :: Word -> m [Word32]
+   getWord32LEs n = replicateM (fromIntegral n) getWord32LE
    -- | Read some Word64 with little-endian order
    getWord64LEs    :: Word -> m [Word64]
+   getWord64LEs n = replicateM (fromIntegral n) getWord64LE
    -- | Read some Word16 with big-endian order
    getWord16BEs    :: Word -> m [Word16]
+   getWord16BEs n = replicateM (fromIntegral n) getWord16BE
    -- | Read some Word32 with big-endian order
    getWord32BEs    :: Word -> m [Word32]
+   getWord32BEs n = replicateM (fromIntegral n) getWord32BE
    -- | Read some Word64 with big-endian order
    getWord64BEs    :: Word -> m [Word64]
+   getWord64BEs n = replicateM (fromIntegral n) getWord64BE
+
    -- | Read the given amount of bytes into a new buffer
    getBuffer     :: Word -> m BufferI
+   getBuffer n = do
+      xs <- replicateM (fromIntegral n) getWord8
+      return (fromListN (fromIntegral n) xs)
+
    -- | Read the given amount of bytes into the specified buffer
    getBufferInto :: Word -> Buffer 'Mutable pin gc heap -> m ()
 
@@ -110,69 +207,69 @@ instance Serializable Word8 where
    type SizeOf Word8  = 'Exactly 1
    type Endian Word8  = 'False
    sizeOf _           = 1
-   put _ x            = putWord8s [x]
-   get _ _            = head <$> getWord8s 1
+   put _ x            = putWord8 x
+   get _ _            = getWord8
 
 instance Serializable Word16 where
    type SizeOf Word16 = 'Exactly 2
    type Endian Word16 = 'True
    sizeOf _           = 2
-   put LittleEndian x = putWord16LEs [x]
-   put BigEndian    x = putWord16BEs [x]
-   get LittleEndian _ = head <$> getWord16LEs 1
-   get BigEndian    _ = head <$> getWord16BEs 1
+   put LittleEndian x = putWord16LE x
+   put BigEndian    x = putWord16BE x
+   get LittleEndian _ = getWord16LE
+   get BigEndian    _ = getWord16BE
 
 instance Serializable Word32 where
    type SizeOf Word32 = 'Exactly 4
    type Endian Word32 = 'True
    sizeOf _           = 4
-   put LittleEndian x = putWord32LEs [x]
-   put BigEndian    x = putWord32BEs [x]
-   get LittleEndian _ = head <$> getWord32LEs 1
-   get BigEndian    _ = head <$> getWord32BEs 1
+   put LittleEndian x = putWord32LE x
+   put BigEndian    x = putWord32BE x
+   get LittleEndian _ = getWord32LE
+   get BigEndian    _ = getWord32BE
 
 instance Serializable Word64 where
    type SizeOf Word64 = 'Exactly 8
    type Endian Word64 = 'True
    sizeOf _           = 8
-   put LittleEndian x = putWord64LEs [x]
-   put BigEndian    x = putWord64BEs [x]
-   get LittleEndian _ = head <$> getWord64LEs 1
-   get BigEndian    _ = head <$> getWord64BEs 1
+   put LittleEndian x = putWord64LE x
+   put BigEndian    x = putWord64BE x
+   get LittleEndian _ = getWord64LE
+   get BigEndian    _ = getWord64BE
 
 instance Serializable Int8 where
    type SizeOf Int8   = 'Exactly 1
    type Endian Int8   = 'False
    sizeOf _           = 1
-   put _ x            = putWord8s [fromIntegral x]
-   get _ _            = fromIntegral . head <$> getWord8s 1
+   put _ x            = putWord8 (fromIntegral x)
+   get _ _            = fromIntegral <$> getWord8
 
 instance Serializable Int16 where
    type SizeOf Int16  = 'Exactly 2
    type Endian Int16  = 'True
    sizeOf _           = 2
-   put LittleEndian x = putWord16LEs [fromIntegral x]
-   put BigEndian    x = putWord16BEs [fromIntegral x]
-   get LittleEndian _ = fromIntegral . head <$> getWord16LEs 1
-   get BigEndian    _ = fromIntegral . head <$> getWord16BEs 1
+   put LittleEndian x = putWord16LE (fromIntegral x)
+   put BigEndian    x = putWord16BE (fromIntegral x)
+   get LittleEndian _ = fromIntegral <$> getWord16LE
+   get BigEndian    _ = fromIntegral <$> getWord16BE
 
 instance Serializable Int32 where
    type SizeOf Int32  = 'Exactly 4
    type Endian Int32  = 'True
    sizeOf _           = 4
-   put LittleEndian x = putWord32LEs [fromIntegral x]
-   put BigEndian    x = putWord32BEs [fromIntegral x]
-   get LittleEndian _ = fromIntegral . head <$> getWord32LEs 1
-   get BigEndian    _ = fromIntegral . head <$> getWord32BEs 1
+   put LittleEndian x = putWord32LE (fromIntegral x)
+   put BigEndian    x = putWord32BE (fromIntegral x)
+   get LittleEndian _ = fromIntegral <$> getWord32LE
+   get BigEndian    _ = fromIntegral <$> getWord32BE
 
 instance Serializable Int64 where
    type SizeOf Int64  = 'Exactly 8
    type Endian Int64  = 'True
    sizeOf _           = 8
-   put LittleEndian x = putWord64LEs [fromIntegral x]
-   put BigEndian    x = putWord64BEs [fromIntegral x]
-   get LittleEndian _ = fromIntegral . head <$> getWord64LEs 1
-   get BigEndian    _ = fromIntegral . head <$> getWord64BEs 1
+   put LittleEndian x = putWord64LE (fromIntegral x)
+   put BigEndian    x = putWord64BE (fromIntegral x)
+   get LittleEndian _ = fromIntegral <$> getWord64LE
+   get BigEndian    _ = fromIntegral <$> getWord64BE
 
 instance Serializable BufferI where
    type SizeOf BufferI = 'Dynamic
---
+++ haskus-binary/src/lib/Haskus/Format/Binary/Serialize/Buffer.hs
@@ -0,0 +1,196 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE BangPatterns #-}
+
+-- | Serializer into a mutable buffer
+--
+-- >>> let w = do putWord8 0x01 ; putWord32BE 0x23456789
+-- >>> b <- newBuffer 10
+-- >>> void $ runBufferPut b 0 w
+-- >>> xs <- forM [0..4] (bufferReadWord8IO b)
+-- >>> xs == [0x01,0x23,0x45,0x67,0x89]
+-- True
+--
+module Haskus.Format.Binary.Serialize.Buffer
+   ( BufferPutT (..)
+   , BufferPut
+   , getPutOffset
+   , getPutBuffer
+   , setPutOffset
+   , runBufferPut
+   )
+where
+
+import Haskus.Format.Binary.Serialize
+import Haskus.Data.Buffer
+import Haskus.Utils.Monad
+
+import Data.Functor.Identity
+import Control.Monad.Trans.State as S
+import Control.Monad.Fail as F
+import Control.Monad.Fix
+
+data BufferPutState b = BufferPutState
+   { bufferPutBuffer :: b    -- ^ Buffer used for writing
+   , bufferPutOffset :: Word -- ^ Current offset
+   } 
+
+newtype BufferPutT m b a
+   = BufferPutT (StateT (BufferPutState b) m a) 
+   deriving newtype (Functor, Applicative, Monad, MonadFail, MonadFix, MonadIO)
+
+type BufferPut b a    = BufferPutT Identity b a
+
+-- | Run a buffer put
+runBufferPut :: Monad m => b -> Word -> BufferPutT m b a -> m (a,Word)
+runBufferPut b off (BufferPutT s) = do
+   (a,s') <- runStateT s (BufferPutState b off)
+   return (a,bufferPutOffset s')
+
+-- | Get current offset
+getPutOffset :: Monad m => BufferPutT m b Word
+getPutOffset = BufferPutT (bufferPutOffset <$> S.get)
+
+-- | Get buffer
+getPutBuffer :: Monad m => BufferPutT m b b
+getPutBuffer = BufferPutT (bufferPutBuffer <$> S.get)
+
+-- | Get current offset
+setPutOffset :: Monad m => Word -> BufferPutT m b ()
+setPutOffset v = BufferPutT $ do
+   S.modify (\s -> s { bufferPutOffset = v })
+
+
+instance
+   ( MonadIO m
+   , MonadFail m
+   ) => PutMonad (BufferPutT m (Buffer 'Mutable pin gc heap))
+   where
+      putWord8 v = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         let !l = off+1
+         when (l > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show l ++ " > " ++ show bs ++ ")"
+
+         bufferWriteWord8IO b off v
+         setPutOffset l
+
+      putWord16 v = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         let !l = off+2
+         when (l > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show l ++ " > " ++ show bs ++ ")"
+
+         bufferWriteWord16IO b off v
+         setPutOffset l
+
+      putWord32 v = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         let !l = off+4
+         when (l > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show l ++ " > " ++ show bs ++ ")"
+
+         bufferWriteWord32IO b off v
+         setPutOffset l
+
+      putWord64 v = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         let !l = off+4
+         when (l > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show l ++ " > " ++ show bs ++ ")"
+
+         bufferWriteWord64IO b off v
+         setPutOffset l
+
+      putWord8s xs = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         let !l = fromIntegral (length xs)
+         when (l+off > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show (l+off) ++ " > " ++ show bs ++ ")"
+
+         forM_ ([off,(off+1)..] `zip` xs) $ \(boff,v) -> do
+            bufferWriteWord8IO b boff v
+
+         setPutOffset (off+l)
+
+      putWord16s xs = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         let !l = 2*fromIntegral (length xs)
+         when (l+off > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show (l+off) ++ " > " ++ show bs ++ ")"
+
+         forM_ ([off,(off+2)..] `zip` xs) $ \(boff,v) -> do
+            bufferWriteWord16IO b boff v
+
+         setPutOffset (off+l)
+
+      putWord32s xs = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         let !l = 4*fromIntegral (length xs)
+         when (l+off > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show (l+off) ++ " > " ++ show bs ++ ")"
+
+         forM_ ([off,(off+4)..] `zip` xs) $ \(boff,v) -> do
+            bufferWriteWord32IO b boff v
+
+         setPutOffset (off+l)
+
+      putWord64s xs = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         let !l = 8*fromIntegral (length xs)
+         when (l+off > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show (l+off) ++ " > " ++ show bs ++ ")"
+
+         forM_ ([off,(off+8)..] `zip` xs) $ \(boff,v) -> do
+            bufferWriteWord64IO b boff v
+
+         setPutOffset (off+l)
+
+      preAllocateAtLeast l = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         when (l+off > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show (l+off) ++ " > " ++ show bs ++ ")"
+
+      -- | FIXME: we should use a faster buffer copy than byte by byte...
+      putBuffer x = do
+         off <- getPutOffset
+         b   <- getPutBuffer
+         bs  <- liftIO (bufferSizeIO b)
+         l   <- liftIO (bufferSizeIO x)
+         when (l+off > bs) $
+            F.fail $ "Not enough space in the target buffer ("
+                   ++ show (l+off) ++ " > " ++ show bs ++ ")"
+
+         forM_ [0..l-1] $ \boff -> do
+            bufferReadWord8IO x boff >>= bufferWriteWord8IO b (boff+off)
+
+         setPutOffset (off+l)
