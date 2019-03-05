3b1b338533a8e348afbc3e3448be134aa3c99c38
Sylvain Henry <sylvain@haskus.fr>
2019-03-05 16:45:12 +0100

    Export Algebra

--- haskus-utils-data/src/lib/Haskus/Utils/Functor.hs
+++ haskus-utils-data/src/lib/Haskus/Utils/Functor.hs
@@ -1,7 +1,10 @@
 -- | Functor helpers
 module Haskus.Utils.Functor
    ( module Data.Functor.Foldable
+   , Algebra
    )
 where
 
 import Data.Functor.Foldable hiding (ListF(..))
+
+type Algebra f a = f a -> a
