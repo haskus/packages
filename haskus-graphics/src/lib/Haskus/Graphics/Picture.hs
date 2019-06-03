136111244369983214769b95f06f468f76a8498c
Sylvain Henry <sylvain@haskus.fr>
2019-06-03 21:38:23 +0200

    Add haskus-graphics package

---
+++ haskus-graphics/src/lib/Haskus/Graphics/Picture.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE RoleAnnotations #-}
+
+module Haskus.Graphics.Picture
+   ( Picture (..)
+   )
+where
+
+import Data.Ratio
+import Haskus.Memory.Buffer
+
+-- | A picture (a 2D array of pixels)
+data Picture p = Picture
+   { pictureWidth      :: !Word      -- ^ Width in pixels
+   , pictureheight     :: !Word      -- ^ Height in pixels
+   , pictureData       :: AnyBuffer  -- ^ Pixel data
+   , picturePixelRatio :: Ratio Word -- ^ Pixel width/height ratio (= 1 if square pixels)
+   }
+
+type role Picture representational
