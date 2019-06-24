0c7a8a50add5c6c3f01d5f349a3d61ec9a8539cc
Sylvain Henry <sylvain@haskus.fr>
2019-06-24 13:02:00 +0200

    Use a Map for named graphs

--- haskus-ui/src/lib/Haskus/UI/Canvas.hs
+++ haskus-ui/src/lib/Haskus/UI/Canvas.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TupleSections #-}
 
+-- | Scene graph and canvas inspired by
+-- * EFL Evas
+-- * Qt QML: https://doc.qt.io/qt-5/qtquick-visualcanvas-scenegraph.html
 module Haskus.UI.Canvas
    ( SceneGraph(..)
    , RenderObject (..)
@@ -9,35 +12,42 @@ module Haskus.UI.Canvas
    , Color (..)
    , Name (..)
    , Canvas (..)
-   , modifyByName
-   , findByName
+   , nodeModifyByName
+   , nodeByName
+   , rootNode
    )
 where
 
 import Haskus.Utils.Monad
 import Haskus.Utils.Maybe
-import Haskus.Utils.Flow
-import Control.Applicative
+import qualified Haskus.Utils.Map as Map
+import Haskus.Utils.Map (Map)
 
 data RenderObject d c
    = Rectangle d d -- ^ Rectangle (width,height,1) with top-left at current (x,y,z)
 
 data Transform d g
    = Translate d d d g -- ^ Translate (x,y,z) the sub-graph in the space
+   -- TODO: use a 4x4 matrix
 
-data SceneGraph d c n
-   = NodeObject (RenderObject d c)              -- ^ Renderable objects
-   | NodeGroup [SceneGraph d c n]               -- ^ NodeGroup of nodes
-   | NodeTransform (Transform d (SceneGraph d c n)) -- ^ Transformations
-   | Colorize c (SceneGraph d c n)              -- ^ Set the default color for the sub-graph
-   | Named n (SceneGraph d c n)                 -- ^ Named sub-graph
-   | NodeClip (SceneGraph d c n) (SceneGraph d c n) -- ^ Object clipping
-   -- | RefNamed n                             -- ^ Reference a sub-graph by its name
+data SceneGraph d c n = SceneGraph
+   { sceneGraphs   :: Map n (SceneNode d c n)
+   , sceneRootName :: n
+   }
+
+data SceneNode d c n
+   = NodeObject (RenderObject d c)                 -- ^ Renderable objects
+   | NodeGroup [SceneNode d c n]                   -- ^ NodeGroup of nodes
+   | NodeTransform (Transform d (SceneNode d c n)) -- ^ Transformations
+   | Colorize c (SceneNode d c n)                  -- ^ Set the default color for the sub-graph
+   | NodeClip (SceneNode d c n) (SceneNode d c n)  -- ^ Object clipping
+   | NodeDisable (SceneNode d c n)                 -- ^ Disable the sub-graph (won't be rendered)
+   | NodeRef n                                     -- ^ Reference a sub-graph by its name
 
 
 -- | Return a color and a Z-order
-colorsAt :: (Num d, Ord d) => d -> d -> SceneGraph d c n -> [(d,c)]
-colorsAt atx aty g = go Nothing 0 atx aty g
+colorsAt :: (Num d, Ord d, Ord n) => d -> d -> SceneGraph d c n -> [(d,c)]
+colorsAt atx aty g = go Nothing 0 atx aty (rootNode g)
    where
       go currentColor z x y = \case
          NodeObject o -> case o of
@@ -49,39 +59,26 @@ colorsAt atx aty g = go Nothing 0 atx aty g
          NodeTransform t -> case t of
             Translate mx my mz g' -> go currentColor (z+mz) (x-mx) (y-my) g'
          Colorize c g'         -> go (Just c) z x y g'
-         Named _ g'            -> go currentColor z x y g'
          NodeClip g' cg
             -- we check that the clipper graph returns a color
             | null (go currentColor z x y cg) -> []
             | otherwise                       -> go currentColor z x y g'
+         NodeDisable _ -> []
+         NodeRef n     -> fromMaybe [] (go currentColor z x y <$> nodeByName n g)
 
 -- | Modify a node by name
-modifyByName :: Eq n => (SceneGraph d c n -> SceneGraph d c n) -> n -> SceneGraph d c n -> SceneGraph d c n
-modifyByName f n g = case g of
-   NodeObject o -> case o of
-      Rectangle _ _      -> g
-   NodeGroup xs          -> NodeGroup (fmap (modifyByName f n) xs)
-   NodeTransform t -> case t of
-      Translate mx my mz g' -> NodeTransform <| Translate mx my mz (modifyByName f n g')
-   Colorize c g'         -> Colorize c (modifyByName f n g')
-   Named n' g'
-      | n == n'   -> Named n' (f g')
-      | otherwise -> Named n' (modifyByName f n g')
-   NodeClip g' cg -> NodeClip (modifyByName f n g') (modifyByName f n cg)
+nodeModifyByName :: (Ord n, Eq n) => (SceneNode d c n -> SceneNode d c n) -> n -> SceneGraph d c n -> SceneGraph d c n
+nodeModifyByName f n g = case nodeByName n g of
+   Nothing -> g
+   Just g' -> g { sceneGraphs = Map.insert n (f g') (sceneGraphs g) }
 
 -- | Find a node by name
-findByName :: Eq n => n -> SceneGraph d c n -> Maybe (SceneGraph d c n)
-findByName n g = case g of
-   NodeObject o -> case o of
-      Rectangle _ _      -> Nothing
-   NodeGroup xs           -> headMaybe (catMaybes (fmap (findByName n) xs))
-   NodeTransform t -> case t of
-      Translate _ _ _ g' -> findByName n g'
-   Colorize _ g'      -> findByName n g'
-   Named n' g'
-      | n == n'   -> Just g'
-      | otherwise -> findByName n g'
-   NodeClip g' cg -> findByName n g' <|> findByName n cg
+nodeByName :: (Ord n, Eq n) => n -> SceneGraph d c n -> Maybe (SceneNode d c n)
+nodeByName n g = Map.lookup n (sceneGraphs g)
+
+-- | Get the root node
+rootNode :: (Ord n) => SceneGraph d c n -> SceneNode d c n
+rootNode g = fromMaybe (NodeGroup []) (nodeByName (sceneRootName g) g)
 
 data Color = Color Float Float Float
 
