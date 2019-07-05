b172e40970c0c71a5eb708748b318a07269f09ce
Sylvain Henry <sylvain@haskus.fr>
2019-07-05 15:48:37 +0200

    Enhance FixedPoint

--- haskus-binary/src/lib/Haskus/Format/Binary/FixedPoint.hs
+++ haskus-binary/src/lib/Haskus/Format/Binary/FixedPoint.hs
@@ -14,7 +14,7 @@
 module Haskus.Format.Binary.FixedPoint
    ( FixedPoint (..)
    , getFixedPointBase
-   , toFixedPointBase
+   , fromFixedPointBase
    , toFixedPoint
    , fromFixedPoint
    )
@@ -24,11 +24,21 @@ import Haskus.Format.Binary.BitField
 import Haskus.Format.Binary.Bits
 import Haskus.Format.Binary.Storable
 import Haskus.Utils.Types
+import Data.Coerce
+import Data.Ratio
 
--- | Fixed-point number
+-- | Unsigned fixed-point number
 -- * `w` is the backing type
 -- * `i` is the number of bits for the integer part (before the radix point)
 -- * `f` is the number of bits for the fractional part (after the radix point)
+--
+-- >>> :set -XDataKinds
+-- >>> import Data.Word
+-- >>> fromIntegral 0 :: FixedPoint Word32 16 16
+-- 0 % 1
+--
+-- >>> fromIntegral 10 :: FixedPoint Word32 16 16
+-- 10 % 1
 newtype FixedPoint w (i :: Nat) (f :: Nat) = FixedPoint (BitFields w
    '[ BitField i "integer"    w
     , BitField f "fractional" w
@@ -40,8 +50,38 @@ getFixedPointBase :: FixedPoint w i f -> w
 getFixedPointBase (FixedPoint (BitFields w)) = w
 
 -- | Set base value
-toFixedPointBase :: forall w i f. w -> FixedPoint w i f
-toFixedPointBase w = FixedPoint @w @i @f (BitFields w)
+fromFixedPointBase :: forall w i f. w -> FixedPoint w i f
+fromFixedPointBase w = FixedPoint @w @i @f (BitFields w)
+
+instance
+   ( BitSize w ~ (i + f)
+   , Num w
+   , FiniteBits w
+   , Bits w
+   , KnownNat i
+   , KnownNat f
+   , Field w
+   , Integral w
+   ) => Num (FixedPoint w i f) where
+   (+)    = coerce ((+) :: w -> w -> w)
+   (-)    = coerce ((-) :: w -> w -> w)
+   negate = error "Can't negate unsigned fixed-point nubmer"
+   abs    = id
+   signum = error "Can't call signum on unsigned fixed-point number"
+   (*)    = error "Fixed-point number multiplication not implemented yet"
+   fromInteger x = toFixedPoint (toRational x)
+
+
+instance
+   ( BitSize w ~ (i + f)
+   , Integral w
+   , FiniteBits w
+   , Bits w
+   , Field w
+   , KnownNat i
+   , KnownNat f
+   ) => Real (FixedPoint w i f) where
+   toRational fp = fromIntegral (getFixedPointBase fp) % (2^(natValue' @f))
 
 deriving instance forall w n d.
    ( Integral w
@@ -52,7 +92,22 @@ deriving instance forall w n d.
    , KnownNat d
    ) => Eq (FixedPoint w n d)
 
-deriving instance forall w n d.
+instance forall w n d.
+   ( Integral w
+   , Bits w
+   , Field w
+   , BitSize w ~ (n + d)
+   , KnownNat n
+   , KnownNat d
+   ) => Ord (FixedPoint w n d) where
+
+   compare x y = compare (getFixedPointBase x) (getFixedPointBase y)
+   x > y       = getFixedPointBase x >  getFixedPointBase y
+   x >= y      = getFixedPointBase x >= getFixedPointBase y
+   x < y       = getFixedPointBase x <  getFixedPointBase y
+   x <= y      = getFixedPointBase x <= getFixedPointBase y
+
+instance forall w n d.
    ( Integral w
    , Bits w
    , Field w
@@ -60,7 +115,9 @@ deriving instance forall w n d.
    , KnownNat n
    , KnownNat d
    , Show w
-   ) => Show (FixedPoint w n d)
+   ) => Show (FixedPoint w n d) where
+
+   show w = show (toRational w)
 
 -- | Convert to a fixed point value
 toFixedPoint :: forall a w (n :: Nat) (d :: Nat).
