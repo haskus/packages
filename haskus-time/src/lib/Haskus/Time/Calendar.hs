2311340f30b8e20432882ec43417e3e5f47bd4d5
Sylvain Henry <sylvain@haskus.fr>
2019-07-22 10:51:30 +0200

    Add support for month lengths

--- haskus-time/src/lib/Haskus/Time/Calendar.hs
+++ haskus-time/src/lib/Haskus/Time/Calendar.hs
@@ -1,19 +1,30 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Haskus.Time.Calendar
    ( Year
    , Month
    , DayOfMonth
    , YearMonthDay (..)
+   , showYearMonthDay
    , isLeapYear
+   , monthLength
+   , nextDay
    )
 where
 
 import Haskus.Format.Binary.Word
 import Haskus.Format.Binary.Bits
+import Haskus.Memory.Embed
+import Haskus.Memory.Buffer
 
 type Year       = Word
+
+-- | Month index [1..12]
 type Month      = Word8
+
+-- | Day of month index [1..31]
 type DayOfMonth = Word8
 
 -- | Gregorian calendar day
@@ -23,6 +34,17 @@ data YearMonthDay = YearMonthDay
    , ymdDay   :: {-# UNPACK #-} !DayOfMonth
    }
    deriving (Show,Eq,Ord)
+
+-- | Show a date as YEAR-MONTH-DAY
+--
+-- >>> showYearMonthDay (YearMonthDay 2017 02 27)
+-- "2017-2-27"
+showYearMonthDay :: YearMonthDay -> String
+showYearMonthDay YearMonthDay{..} = mconcat
+   [ show ymdYear , "-"
+   , show ymdMonth, "-"
+   , show ymdDay
+   ]
 
 -- | In the Gregorian calendar, a year has an additional leap day if:
 --    1) it is divisible by 4 and not by 100
@@ -39,3 +61,38 @@ data YearMonthDay = YearMonthDay
 isLeapYear :: Year -> Bool
 isLeapYear y = y .&. 3 == 0 && (r100 /= 0 || q100 .&. 3 == 0) where
    (q100, r100) = y `quotRem` 100
+
+-- | Table of number of days in each month (as Word8)
+monthDaysTable :: BufferE
+monthDaysTable = $$(embedBytes [31,28,31,30,31,30,31,31,30,31,30,31])
+
+-- | Table of number of days in each month for a leap year (as Word8)
+monthDaysLeapTable :: BufferE
+monthDaysLeapTable = $$(embedBytes [31,29,31,30,31,30,31,31,30,31,30,31])
+
+-- | Get month length
+--
+-- >>> monthLength 2019 2
+-- 28
+-- >>> monthLength 2016 2
+-- 29
+monthLength :: Year -> Month -> Word8
+monthLength y m = bufferReadWord8 table (fromIntegral m - 1)
+   where
+      table
+         | isLeapYear y = monthDaysLeapTable
+         | otherwise    = monthDaysTable
+
+-- | Get the next day
+--
+-- >>> showYearMonthDay (nextDay (YearMonthDay 2019 12 31))
+-- "2020-1-1"
+-- >>> showYearMonthDay (nextDay (YearMonthDay 2019 2  28))
+-- "2019-3-1"
+-- >>> showYearMonthDay (nextDay (YearMonthDay 2016 2  28))
+-- "2016-2-29"
+nextDay :: YearMonthDay -> YearMonthDay
+nextDay YearMonthDay{..}
+   | ymdDay < monthLength ymdYear ymdMonth = YearMonthDay ymdYear ymdMonth (ymdDay+1)
+   | ymdMonth == 12                        = YearMonthDay (ymdYear+1) 1 1
+   | otherwise                             = YearMonthDay ymdYear (ymdMonth+1) 1
