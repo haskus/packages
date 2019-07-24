8b493b40ee8b5838b703d82fac5dc393aad909a0
Sylvain Henry <sylvain@haskus.fr>
2019-07-24 21:07:19 +0200

    MonadTree: don't store a value in Monadic nodes

--- haskus-utils/src/lib/Haskus/Utils/MonadFlow.hs
+++ haskus-utils/src/lib/Haskus/Utils/MonadFlow.hs
@@ -11,20 +11,16 @@ module Haskus.Utils.MonadFlow
    , runMonadFlow
    , runM
    , withM
-   , scopeM
    , emitM
    -- * Cached control flow
    , CachedMonadFlow (..)
    , cacheMonadFlow
    , cacheMonadFlowPure
    , updateCachedMonadFlow
-   , updateCachedMonadFlowMaybe
    , monadFlowToMonadTree
    )
 where
 
-import Haskus.Utils.Either
-import Haskus.Utils.Maybe
 import Haskus.Utils.Flow
 import Haskus.Utils.MonadVar
 import Haskus.Utils.MonadTree
@@ -35,24 +31,18 @@ data MonadFlowF m a e
    = MonadEdmit a e                                               -- emit a pure value
    | forall v. Eq v => MonadRead (m v) (v -> e)                  -- read a monadic value and put it in the current scope
    | forall v. Eq v => MonadWith (m v) (v -> MonadFlow m a ()) e -- open a new scope and read a monadic value in it
-   | MonadScope (MonadFlow m a ())  e                            -- open a new scope
 
 type MonadFlow m a r = Free (MonadFlowF m a) r
 
 instance Functor (MonadFlowF m a) where
    fmap f = \case
-      MonadEdmit a e   -> MonadEdmit a (f e)
+      MonadEdmit a e  -> MonadEdmit a (f e)
       MonadRead v g   -> MonadRead v (f . g)
-      MonadScope k e  -> MonadScope k (f e)
       MonadWith v k e -> MonadWith v k (f e)
 
 -- | Run an MonadFlow
 runMonadFlow :: Monad m => MonadFlow m a r -> m (r,[a])
 runMonadFlow = \case
-   Free (MonadScope t1 t2) -> do
-      (_,r1)  <- runMonadFlow t1
-      (k2,r2) <- runMonadFlow t2
-      pure (k2, r1 <> r2)
    Free (MonadWith io f t) -> do
       val <- io
       (_,r1)  <- runMonadFlow (f val)
@@ -71,10 +61,6 @@ runMonadFlow = \case
 -- | Emit a pure value
 emitM :: a -> MonadFlow m a ()
 emitM a = liftF (MonadEdmit a ())
-
--- | Create a new scope
-scopeM :: MonadFlow m a () -> MonadFlow m a ()
-scopeM a = liftF (MonadScope a ())
 
 -- | Get a variable in IO
 --
@@ -111,28 +97,14 @@ cacheMonadFlowPure ctx f = (CachedMonadFlow (monadFlowToMonadTree f) ctx)
 
 -- | Update a cached MonadFlow
 updateCachedMonadFlow :: Monad m => CachedMonadFlow m a -> m (CachedMonadFlow m a)
-updateCachedMonadFlow cflow = do
-   cflow' <- updateCachedMonadFlowMaybe cflow
-   pure (fromMaybe cflow cflow')
-
--- | Update a cached MonadFlow
---
--- Return Nothing if nothing changed
-updateCachedMonadFlowMaybe :: Monad m => CachedMonadFlow m a -> m (Maybe (CachedMonadFlow m a))
-updateCachedMonadFlowMaybe (CachedMonadFlow trees withCtx) = do
+updateCachedMonadFlow (CachedMonadFlow trees withCtx) = do
    trees' <- withCtx (forM trees updateMonadTree)
-   if all isLeft trees'
-      then pure Nothing
-      else do
-         let rOrL (Right x) = x
-             rOrL (Left x)  = x
-         pure (Just (CachedMonadFlow (rOrL <|| trees') withCtx))
+   pure (CachedMonadFlow trees' withCtx)
 
 monadFlowToMonadTree :: MonadFlow m a r -> [MonadTree m a]
 monadFlowToMonadTree = \case
-   Free (MonadScope t1 t2) -> StaticBranch (Node Nothing (monadFlowToMonadTree t1)): monadFlowToMonadTree t2
-   Free (MonadRead io f)   -> [ MonadBranch (MonadVar io (Node Nothing . monadFlowToMonadTree . f)) ]
-   Free (MonadWith io f c) -> MonadBranch (MonadVar io (Node Nothing . monadFlowToMonadTree . f)):monadFlowToMonadTree c
-   Free (MonadEdmit a t)   -> StaticBranch (Node (Just a) []):monadFlowToMonadTree t
+   Free (MonadRead io f)   -> [ MonadBranch (MonadVar io (monadFlowToMonadTree . f)) ]
+   Free (MonadWith io f c) -> MonadBranch (MonadVar io (monadFlowToMonadTree . f)):monadFlowToMonadTree c
+   Free (MonadEdmit a t)   -> StaticBranch a []:monadFlowToMonadTree t
    Pure _                  -> []
 
--- haskus-utils/src/lib/Haskus/Utils/MonadTree.hs
+++ haskus-utils/src/lib/Haskus/Utils/MonadTree.hs
@@ -6,7 +6,6 @@
 -- | Monadic tree with cache
 module Haskus.Utils.MonadTree
    ( MonadTree (..)
-   , Node (..)
    , showMonadTree
    , showMonadTrees
    , updateMonadTree
@@ -14,68 +13,57 @@ module Haskus.Utils.MonadTree
 where
 
 import Haskus.Utils.Monad
-import Haskus.Utils.Either
 import Haskus.Utils.MonadVar
 
 -- | Monad dependent rose tree
 --
 -- Both the tree values and the tree nodes may be monadically dependent.
 data MonadTree m a
-   = StaticBranch (Node m a)                                 -- ^ Static node
-   | forall s. Eq s => MonadBranch (MonadVar m s (Node m a)) -- ^ Monad dependent node
+   = StaticBranch a [MonadTree m a]                               -- ^ Static node
+   | forall s. Eq s => MonadBranch (MonadVar m s [MonadTree m a]) -- ^ Monad dependent nodes
 
 deriving instance Functor (MonadTree m)
-
-data Node m a = Node (Maybe a) [MonadTree m a]
-   deriving (Functor)
 
 -- | Pretty-show an MonadTree
 showMonadTree :: Show a => MonadTree m a -> String
 showMonadTree = go 0
    where
-      indent isIO b n = replicate (2*n) ' ' ++ if b then "+ " else (if isIO then "* " else "- ")
-      showNode isIO b n = \case
-         Node Nothing ts  -> indent isIO b n <> "()\n" <> concatMap (go (n+1)) ts
-         Node (Just a) ts -> indent isIO b n <> show a <> "\n" <> concatMap (go (n+1)) ts
+      indent n c      = replicate (2*n) ' ' <> c
+      showNode n a ts = indent n "- " <> show a <> "\n" <> concatMap (go (n+1)) ts
       go n = \case
-         StaticBranch nd                 -> showNode False False n nd
-         MonadBranch (MonadVar {})             -> indent True True n <> "...\n"
-         MonadBranch (CachedMonadVar nd _ _ _) -> showNode True False n nd
+         StaticBranch a ts                     -> showNode n a ts
+         MonadBranch (MonadVar {})             -> indent n "{...}\n"
+         MonadBranch (CachedMonadVar ts _ _ _) -> indent n "{" <> concatMap (go (n+1)) ts <> indent n "}\n"
 
 -- | Pretty-show some MonadTrees
 showMonadTrees :: Show a => [MonadTree m a] -> String
 showMonadTrees = concatMap showMonadTree
 
+data Diff a
+   = Same a    -- ^ Has not changed
+   | Updated a -- ^ May have changed
+   deriving (Show,Eq,Ord)
+
 -- | Update a MonadTree recursively. Reuse cached values when possible
 --
 -- Left return: nothing has changed in the tree
 -- Right return: the tree has changed
-updateMonadTree :: Monad m => MonadTree m a -> m (Either (MonadTree m a) (MonadTree m a))
-updateMonadTree node = case node of
-   StaticBranch nd -> updateNode nd >>= \case
-                     Left _    -> pure (Left node)
-                     Right nd' -> pure (Right (StaticBranch nd'))
-   MonadBranch dv -> case dv of
-      MonadVar io f -> do
-         ~cdv@(CachedMonadVar a s _ _) <- updateMonadVarForce dv
-         updateNode a >>= \case
-            Left _   -> pure (Right (MonadBranch cdv))
-            Right a' -> pure (Right (MonadBranch (CachedMonadVar a' s io f)))
-      CachedMonadVar a s io f -> do
-         mcdv <- updateMonadVarMaybe dv
-         case mcdv of
-            Nothing -> do
-               updateNode a >>= \case
-                  Left  _  -> pure (Left node)
-                  Right a' -> pure (Right (MonadBranch (CachedMonadVar a' s io f)))
-            Just ~cdv@(CachedMonadVar a' s' _ _) -> do
-               updateNode a' >>= \case
-                  Left _    -> pure (Right (MonadBranch cdv))
-                  Right a'' -> pure (Right (MonadBranch (CachedMonadVar a'' s' io f)))
-         
+updateMonadTree :: Monad m => MonadTree m a -> m (MonadTree m a)
+updateMonadTree = go
    where
-      updateNode nd@(Node a ts) = do
-         ts' <- forM ts updateMonadTree
-         if null ts' || all isLeft ts'
-            then pure (Left  nd)
-            else pure (Right (Node a (fmap fromEither ts')))
+      go node = case node of
+            StaticBranch a ts -> StaticBranch a <$> forM ts go
+            MonadBranch dv -> case dv of
+               MonadVar io f -> do
+                  ~(CachedMonadVar ts s _ _) <- updateMonadVarForce dv
+                  ts' <- forM ts go
+                  pure (MonadBranch (CachedMonadVar ts' s io f))
+               CachedMonadVar ts s io f -> do
+                  mcdv <- updateMonadVarMaybe dv
+                  case mcdv of
+                     Nothing -> do
+                        ts' <- forM ts go
+                        pure (MonadBranch (CachedMonadVar ts' s io f))
+                     Just ~(CachedMonadVar ts' s' _ _) -> do
+                        ts'' <- forM ts' go
+                        pure (MonadBranch (CachedMonadVar ts'' s' io f))
