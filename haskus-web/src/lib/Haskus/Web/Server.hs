6a12067b8417daff20065fc86e411fe5a0e87c44
Sylvain Henry <sylvain@haskus.fr>
2020-06-06 22:21:42 +0200

    Re-export WAI/Warp stuff

---
+++ haskus-web/src/lib/Haskus/Web/Server.hs
@@ -0,0 +1,14 @@
+module Haskus.Web.Server
+   ( module Network.Wai.Handler.Warp
+   , module Network.Wai.Handler.WarpTLS
+   , module Network.Wai
+   , module Network.Wai.Internal
+   , module Network.HTTP.Types
+   )
+where
+
+import Network.Wai.Handler.Warp
+import Network.Wai.Handler.WarpTLS
+import Network.Wai
+import Network.Wai.Internal
+import Network.HTTP.Types
