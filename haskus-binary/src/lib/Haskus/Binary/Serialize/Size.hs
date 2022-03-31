ab6977469a02d308aa3cd08d73af874aa8bb53b2
Sylvain Henry <sylvain@haskus.fr>
2022-03-31 14:13:48 +0200

    Remove one use of transformers' State

--- haskus-binary/src/lib/Haskus/Binary/Serialize/Size.hs
+++ haskus-binary/src/lib/Haskus/Binary/Serialize/Size.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
 
 module Haskus.Binary.Serialize.Size
    ( GetSize (..)
@@ -13,20 +15,35 @@ where
 
 import Haskus.Binary.Serialize.Put
 import Haskus.Memory.Buffer
-import Control.Monad.Trans.State.Strict as S
 import System.IO.Unsafe
+import GHC.Exts
 
 newtype GetSize a
-   = GetSize (State Word a) 
-   deriving newtype (Functor, Applicative, Monad)
+   = GetSize (Word# -> (# Word#, a #))
+
+instance Functor GetSize where
+  fmap f (GetSize g) = GetSize \w0 -> case g w0 of
+                          (# w1, a #) -> (# w1, f a #)
+
+instance Applicative GetSize where
+  pure a = GetSize \w -> (# w, a #)
+  GetSize f <*> GetSize a = GetSize \w0 -> case f w0 of
+                              (# w1, f' #) -> case a w1 of
+                                (# w2, a' #) -> (# w2, f' a' #)
+
+instance Monad GetSize where
+  GetSize m >>= f = GetSize \w0 -> case m w0 of
+    (# w1, a #) -> case f a of
+      GetSize f' -> f' w1
 
 -- | Increment the current size
 incSize :: Word -> GetSize ()
-incSize x = GetSize (state (\s -> ((),s+x)))
+incSize (W# x) = GetSize \w -> (# w `plusWord#` x, () #)
 
 -- | Get the total size
 runGetSize :: GetSize a -> Word
-runGetSize (GetSize s) = execState s 0
+runGetSize (GetSize s) = case s 0## of
+  (# w, _a #) -> W# w
 
 instance PutMonad GetSize where
    putWord8 _  = incSize 1
