a033b93c6443294cabbb8b6dcd2c9d36e4d17adf
Sylvain Henry <sylvain@haskus.fr>
2024-08-06 23:32:48 +0200

    Binary: add SizedWriter

--- haskus-binary/src/lib/Haskus/Memory/Writer.hs
+++ haskus-binary/src/lib/Haskus/Memory/Writer.hs
@@ -1,5 +1,7 @@
 module Haskus.Memory.Writer
   ( Writer (..)
+  , SizedWriter (..)
+  -- * writers
   , writeWord8#
   , writeWord16#
   , writeWord16BE#
@@ -20,6 +22,27 @@ module Haskus.Memory.Writer
   , writeInt64#
   , writeInt64BE#
   , writeInt64LE#
+  -- * Sized writers
+  , swriteWord8#
+  , swriteWord16#
+  , swriteWord16BE#
+  , swriteWord16LE#
+  , swriteWord32#
+  , swriteWord32BE#
+  , swriteWord32LE#
+  , swriteWord64#
+  , swriteWord64BE#
+  , swriteWord64LE#
+  , swriteInt8#
+  , swriteInt16#
+  , swriteInt16BE#
+  , swriteInt16LE#
+  , swriteInt32#
+  , swriteInt32BE#
+  , swriteInt32LE#
+  , swriteInt64#
+  , swriteInt64BE#
+  , swriteInt64LE#
   )
 where
 
@@ -60,6 +83,33 @@ instance Semigroup (Writer s) where
 instance Monoid (Writer s) where
   {-# INLINE mempty #-}
   mempty = Writer \addr s -> (# s, addr #)
+
+
+-- | A writer that carries the size of the written value in bytes.
+data SizedWriter s
+  = SizedWriter !Word# !(Writer s)
+
+instance Semigroup (SizedWriter s) where
+  SizedWriter n f <> SizedWriter m g
+    = SizedWriter (n `plusWord#` m) (f <> g)
+
+instance Monoid (SizedWriter s) where
+  {-# INLINE mempty #-}
+  mempty = SizedWriter 0## mempty
+  {-# INLINE mconcat #-}
+  mconcat sws = SizedWriter (sum_size 0## sws) (sum_writer mempty sws)
+    where
+      sum_size n = \case
+        [] -> n
+        SizedWriter s _ : sw -> sum_size (n `plusWord#` s) sw
+      sum_writer f = \case
+        [] -> f
+        SizedWriter _ g : sw -> sum_writer (f <> g) sw
+
+
+----------------------------
+-- Writers
+----------------------------
 
 writeWord8# :: Word8# -> Writer s
 writeWord8# w = Writer \addr s ->
@@ -160,3 +210,67 @@ writeInt64LE# :: Int64# -> Writer s
 writeInt64LE# w = case hostEndianness of
   LittleEndian -> writeInt64# w
   BigEndian -> writeInt64# (byteSwapInt64# w)
+
+----------------------------
+-- Sized Writers
+----------------------------
+
+swriteWord8# :: Word8# -> SizedWriter s
+swriteWord8# w = SizedWriter 1## (writeWord8# w)
+
+swriteWord16# :: Word16# -> SizedWriter s
+swriteWord16# w = SizedWriter 2## (writeWord16# w)
+
+swriteWord16BE# :: Word16# -> SizedWriter s
+swriteWord16BE# w = SizedWriter 2## (writeWord16BE# w)
+
+swriteWord16LE# :: Word16# -> SizedWriter s
+swriteWord16LE# w = SizedWriter 2## (writeWord16LE# w)
+
+swriteWord32# :: Word32# -> SizedWriter s
+swriteWord32# w = SizedWriter 4## (writeWord32# w)
+
+swriteWord32BE# :: Word32# -> SizedWriter s
+swriteWord32BE# w = SizedWriter 4## (writeWord32BE# w)
+
+swriteWord32LE# :: Word32# -> SizedWriter s
+swriteWord32LE# w = SizedWriter 4## (writeWord32LE# w)
+
+swriteWord64# :: Word64# -> SizedWriter s
+swriteWord64# w = SizedWriter 8## (writeWord64# w)
+
+swriteWord64BE# :: Word64# -> SizedWriter s
+swriteWord64BE# w = SizedWriter 8## (writeWord64BE# w)
+
+swriteWord64LE# :: Word64# -> SizedWriter s
+swriteWord64LE# w = SizedWriter 8## (writeWord64LE# w)
+
+swriteInt8# :: Int8# -> SizedWriter s
+swriteInt8# w = SizedWriter 1## (writeInt8# w)
+
+swriteInt16# :: Int16# -> SizedWriter s
+swriteInt16# w = SizedWriter 2## (writeInt16# w)
+
+swriteInt16BE# :: Int16# -> SizedWriter s
+swriteInt16BE# w = SizedWriter 2## (writeInt16BE# w)
+
+swriteInt16LE# :: Int16# -> SizedWriter s
+swriteInt16LE# w = SizedWriter 2## (writeInt16LE# w)
+
+swriteInt32# :: Int32# -> SizedWriter s
+swriteInt32# w = SizedWriter 4## (writeInt32# w)
+
+swriteInt32BE# :: Int32# -> SizedWriter s
+swriteInt32BE# w = SizedWriter 4## (writeInt32BE# w)
+
+swriteInt32LE# :: Int32# -> SizedWriter s
+swriteInt32LE# w = SizedWriter 4## (writeInt32LE# w)
+
+swriteInt64# :: Int64# -> SizedWriter s
+swriteInt64# w = SizedWriter 8## (writeInt64# w)
+
+swriteInt64BE# :: Int64# -> SizedWriter s
+swriteInt64BE# w = SizedWriter 8## (writeInt64BE# w)
+
+swriteInt64LE# :: Int64# -> SizedWriter s
+swriteInt64LE# w = SizedWriter 8## (writeInt64LE# w)
