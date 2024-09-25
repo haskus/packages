895594d8e0a78d02f3dbce23572f3280f7e4b267
Sylvain Henry <sylvain@haskus.fr>
2024-09-25 17:09:37 +0200

    x86: refactoring

--- haskus-system/src/lib/Haskus/Arch/X86_64/ISA/Encoding/ModRM.hs
+++ haskus-system/src/lib/Haskus/Arch/X86_64/ISA/Encoding/ModRM.hs
@@ -3,10 +3,10 @@ module Haskus.Arch.X86_64.ISA.Encoding.ModRM
   ( ModRM
   , writeModRM
   , mkModRM
-  , mkModRM_ext
-  , mkModRM_ext_reg
   , mkModRM_reg
-  , mkModRM_regs_reg_rm
+  , mkModRM_rm
+  , mkModRM_mod
+
   , mkModRM_mod_rm
   )
 where
@@ -29,21 +29,19 @@ instance Monoid ModRM where
 mkModRM :: U8 -> U8 -> U8 -> ModRM
 mkModRM m r a = ModRM ((m `shiftL` 6) .|. (r `shiftL` 3) .|. a)
 
--- | Store opcode extension in ModRM.reg and register in ModRM.rm
-mkModRM_ext_reg :: U8 -> U8 -> ModRM
-mkModRM_ext_reg ext r = ModRM (0b11_000_000 .|. (ext `shiftL` 3) .|. r)
+-- | Store value in ModRM.rm
+mkModRM_rm :: U8 -> ModRM
+mkModRM_rm v = ModRM v
 
--- | Store register in ModRM.reg
+-- | Store value in ModRM.reg
 mkModRM_reg :: U8 -> ModRM
-mkModRM_reg r = ModRM r
+mkModRM_reg v = ModRM (v `shiftL` 3)
 
--- | Store opcode extension in ModRM.reg
-mkModRM_ext :: U8 -> ModRM
-mkModRM_ext ext = ModRM (ext `shiftL` 3)
+-- | Store value in ModRM.mod
+mkModRM_mod :: U8 -> ModRM
+mkModRM_mod m = ModRM (m `shiftL` 6)
 
-mkModRM_regs_reg_rm :: U8 -> U8 -> ModRM
-mkModRM_regs_reg_rm r m = ModRM (0b11_000_000 .|. (r `shiftL` 3) .|. m)
-
+-- | Store value in ModRM.mod and ModRM.rm
 mkModRM_mod_rm :: U8 -> U8 -> ModRM
 mkModRM_mod_rm m rm = ModRM (m `shiftL` 6 .|. rm)
 
