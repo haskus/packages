abfc79c238f9bd36a41c937d90c6ae33eeb9629d
Sylvain Henry <sylvain@haskus.fr>
2024-10-17 22:12:44 +0200

    x86: ADDSUBPx

--- haskus-system/src/lib/Haskus/Arch/X86_64/ISA/Encoding/Vex.hs
+++ haskus-system/src/lib/Haskus/Arch/X86_64/ISA/Encoding/Vex.hs
@@ -6,6 +6,10 @@ module Haskus.Arch.X86_64.ISA.Encoding.Vex
   , mkVex_LIG_F3_0F_WIG
   , mkVex_128_66_0F_WIG
   , mkVex_256_66_0F_WIG
+  , mkVex_128_F2_0F_WIG
+  , mkVex_256_F2_0F_WIG
+  , mkVex_128_F3_0F_WIG
+  , mkVex_256_F3_0F_WIG
   , mkVex_128_0F_WIG
   , mkVex_256_0F_WIG
   , extendVex
@@ -28,6 +32,10 @@ mkVex_LIG_F2_0F_WIG :: Vex
 mkVex_LIG_F3_0F_WIG :: Vex
 mkVex_128_66_0F_WIG :: Vex
 mkVex_256_66_0F_WIG :: Vex
+mkVex_128_F2_0F_WIG :: Vex
+mkVex_256_F2_0F_WIG :: Vex
+mkVex_128_F3_0F_WIG :: Vex
+mkVex_256_F3_0F_WIG :: Vex
 mkVex_128_0F_WIG :: Vex
 mkVex_256_0F_WIG :: Vex
 
@@ -35,6 +43,10 @@ mkVex_LIG_F2_0F_WIG = Vex2 0b1_1111_0_11 -- R=1, vvvv=1111, L=0, pp=11
 mkVex_LIG_F3_0F_WIG = Vex2 0b1_1111_0_11 -- R=1, vvvv=1111, L=0, pp=10
 mkVex_128_66_0F_WIG = Vex2 0b1_1111_0_01 -- R=1, vvvv=1111, L=0, pp=01
 mkVex_256_66_0F_WIG = Vex2 0b1_1111_1_01 -- R=1, vvvv=1111, L=1, pp=01
+mkVex_128_F2_0F_WIG = Vex2 0b1_1111_0_11 -- R=1, vvvv=1111, L=0, pp=11
+mkVex_256_F2_0F_WIG = Vex2 0b1_1111_1_11 -- R=1, vvvv=1111, L=1, pp=11
+mkVex_128_F3_0F_WIG = Vex2 0b1_1111_0_10 -- R=1, vvvv=1111, L=0, pp=10
+mkVex_256_F3_0F_WIG = Vex2 0b1_1111_1_10 -- R=1, vvvv=1111, L=1, pp=10
 mkVex_128_0F_WIG    = Vex2 0b1_1111_0_00 -- R=1, vvvv=1111, L=0, pp=00
 mkVex_256_0F_WIG    = Vex2 0b1_1111_1_00 -- R=1, vvvv=1111, L=1, pp=00
 
